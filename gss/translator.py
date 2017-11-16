9a93c37c1e667b1a23fc8de231fd17e4c4b7b5f0
Nurasyl Aldan <nurassyl.aldan@gmail.com>
2017-11-17 02:58:08 +0600

    Translator created.

--- gss/translator.py
+++ gss/translator.py
@@ -1,9 +1,49 @@
 from gss.dictionary import *
+from gss.analyzer import *
+from gss.utils import *
 
-def translate_cyrillic_to_latin(text, dictionary):
+# letters: ('cyrillic', 'latin', 'tote')
+def __translate(text, _dict, letters='cyrillic'):
     result = {
-        'not_found': list(),
+        'unknowns': list(),
         'output': ''
     }
+
+    if letters == 'latin':
+        words = get_all_latin_words(text)
+    elif letters == 'tote':
+        words = get_all_tote_words(text)
+    else:
+        words = get_all_cyrillic_words(text)
+
+    words.sort(key = len, reverse=True)
+
+    for word in words:
+        try:
+            wc = what_case(word)
+            text = text.replace(word, normalize(_dict[word.lower()], wc))
+        except KeyError:
+            result['unknowns'].append(word)
+        del word, wc
+    
+    if letters == 'tote':
+        text = text.lower()
+
     result['output'] = text
+    del text, _dict, words
     return result
+
+def translate_cyrillic_to_latin(text, _dict):
+    return __translate(text, _dict)
+def translate_cyrillic_to_tote(text, _dict):
+    return __translate(text, _dict)
+
+def translate_latin_to_cyrillic(text, _dict):
+    return __translate(text, _dict, 'latin')
+def translate_latin_to_tote(text, _dict):
+    return __translate(text, _dict, 'latin')
+
+def translate_tote_to_latin(text, _dict):
+    return __translate(text, _dict, 'tote')
+def translate_tote_to_cyrillic(text, _dict):
+    return __translate(text, _dict, 'tote')
--- test.py
+++ test.py
@@ -1,4 +1,5 @@
 from gss.dictionary import *
+from gss.translator import *
 
 # 1 сөз = 100 байт.
 # 10 сөз = 1 Кбайт.
@@ -8,7 +9,19 @@ from gss.dictionary import *
 
 words = [
     'қазақ',
-    'электр'
+    'тілі',
 ]
 
-print(create_dict_tote_cyrillic(words))
+cyrillic_latin_dict = create_dict_cyrillic_latin(words)
+cyrillic_tote_dict = create_dict_cyrillic_tote(words)
+latin_cyrillic_dict = create_dict_latin_cyrillic(words)
+latin_tote_dict = create_dict_latin_tote(words)
+tote_latin_dict = create_dict_tote_latin(words)
+tote_cyrillic_dict = create_dict_tote_cyrillic(words)
+
+print(translate_cyrillic_to_latin('Қазақ тілі', cyrillic_latin_dict))
+print(translate_cyrillic_to_tote('Қазақ тілі', cyrillic_tote_dict))
+print(translate_latin_to_cyrillic('Qazaq tili', latin_cyrillic_dict))
+print(translate_latin_to_tote('Qazaq tili', latin_tote_dict))
+print(translate_tote_to_latin('قازاق تىلى', tote_latin_dict))
+print(translate_tote_to_cyrillic('قازاق تىلى', tote_cyrillic_dict))
